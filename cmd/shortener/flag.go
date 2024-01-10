310f664c8da2c8ca5610fb43b2b8ea52d82d3143
Karinka-malinka <k.malutina@unistar.ru>
2024-01-10 13:04:56 +0300

    iter5

--- cmd/shortener/flag.go
+++ cmd/shortener/flag.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 
 	"github.com/URL_shortener/internal/config"
 )
@@ -12,4 +13,12 @@ func parseFlags(cfg *config.ConfigData) {
 	flag.StringVar(&cfg.BaseShortAddr, "b", "http://localhost:8080", "base address of the resulting shortened URL")
 
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
+		cfg.RunAddr = envRunAddr
+	}
+
+	if envBaseAddr := os.Getenv("BASE_URL"); envBaseAddr != "" {
+		cfg.BaseShortAddr = envBaseAddr
+	}
 }
